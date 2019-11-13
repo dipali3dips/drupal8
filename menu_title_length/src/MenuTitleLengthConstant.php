0d03f47ff7e291ebdf7cf758b8051c73dea6e972
Dipali Goel <dipali_g@photonint.com>
2019-11-13 16:01:46 +0530

    adding drupal 8 module menu title length which allows us to set the lenth for menu title

---
+++ menu_title_length/src/MenuTitleLengthConstant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\menu_title_length;
+
+/**
+ * Add constants to use across module.
+ */
+final class MenuTitleLengthConstant {
+
+  /**
+   * Set Constants for Menu Title Length.
+   */
+
+  const MENU_TITLE_LENGTH = 20;
+
+}
